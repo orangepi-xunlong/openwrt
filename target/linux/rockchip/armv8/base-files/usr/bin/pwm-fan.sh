7a4b79f6789841b93ce1baa6909172dd3e7fba22
aagu <aaguisme@gmail.com>
2022-05-02 11:22:43 +0800

    Spliting threshold of fan starting/stopping
    Frequently changes of cpu temperatrue will make fan to start/stop in a very short time
    
    The init.d script for pwm-fan is polished too, a simple implement of stop function is made

--- target/linux/rockchip/armv8/base-files/usr/bin/pwm-fan.sh
+++ target/linux/rockchip/armv8/base-files/usr/bin/pwm-fan.sh
@@ -17,48 +17,55 @@ ISENABLE=`cat /sys/class/pwm/pwmchip0/pwm0/enable`
 if [ $ISENABLE -eq 1 ]; then
     echo -n 0 > /sys/class/pwm/pwmchip0/pwm0/enable
 fi
-echo -n 1000000 > /sys/class/pwm/pwmchip0/pwm0/period
+
+#20Hz
+echo -n 50000 > /sys/class/pwm/pwmchip0/pwm0/period
 echo -n 1 > /sys/class/pwm/pwmchip0/pwm0/enable
 
 # max speed run 5s
-echo -n 500000 > /sys/class/pwm/pwmchip0/pwm0/duty_cycle
+echo -n 46990 > /sys/class/pwm/pwmchip0/pwm0/duty_cycle
 sleep 5
-echo -n 500000 > /sys/class/pwm/pwmchip0/pwm0/duty_cycle
+echo -n 25000 > /sys/class/pwm/pwmchip0/pwm0/duty_cycle
 
 # declare -a CpuTemps=(55000 43000 38000 32000)
 # declare -a PwmDutyCycles=(1000 20000 30000 45000)
 
-declare -a CpuTemps=(75000 63000 58000 52000)
-declare -a PwmDutyCycles=(500000 650000 800000 950000)
+declare -a CpuTemps=(75000 63000 58000 55000)
+declare -a CpuTempsLow=(70000 58000 53000 50000)
+declare -a PwmDutyCycles=(10000 25000 35000 46990)
 
 declare -a Percents=(100 75 50 25)
-DefaultDuty=999990
+DefaultDuty=49990
 DefaultPercents=0
 
 while true
 do
-	temp=$(cat /sys/class/thermal/thermal_zone0/temp)
-	INDEX=0
-	FOUNDTEMP=0
-	DUTY=$DefaultDuty
-	PERCENT=$DefaultPercents
+        temp=$(cat /sys/class/thermal/thermal_zone0/temp)
+        DUTY=$(cat /sys/class/pwm/pwmchip0/pwm0/duty_cycle)
+        PERCENT=$DefaultPercents
+        new_duty=$DefaultDuty
 
-	for i in 0 1 2 3; do
-		if [ $temp -gt ${CpuTemps[$i]} ]; then
-			INDEX=$i
-			FOUNDTEMP=1
-			break
-		fi
-	done
-	if [ ${FOUNDTEMP} == 1 ]; then
-		DUTY=${PwmDutyCycles[$i]}
-		PERCENT=${Percents[$i]}
-	fi
+        for i in 0 1 2 3; do
+                if [ $temp -gt ${CpuTemps[$i]} ]; then
+                        new_duty=${PwmDutyCycles[$i]}
+                        break
+                fi
+        done
 
-	echo -n $DUTY > /sys/class/pwm/pwmchip0/pwm0/duty_cycle;
+        # check if we should slow down the fan speed
+        if [ $new_duty -gt ${DUTY} ]; then
+            if [ $temp -lt ${CpuTempsLow[$i]} ]; then
+                # temp has dropped to the low barrier, slow down
+                DUTY=$new_duty
+            fi
+        else
+            DUTY=$new_duty
+        fi
+
+        echo -n $DUTY > /sys/class/pwm/pwmchip0/pwm0/duty_cycle;
 
         # echo "temp: $temp, duty: $DUTY, ${PERCENT}%"
         # cat /sys/devices/system/cpu/cpu*/cpufreq/cpuinfo_cur_freq
 
-	sleep 2s;
+        sleep 2s;
 done
